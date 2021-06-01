3f2202c240fe0729b21f3645dd4324f47919999b
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-06-01 22:10:30 +0100

    Remove LZMA and add ZSTD
    When .xz is present lzma will never be used, it's old and redundant. ZSTD is the future :)

--- scan-packages.sh
+++ scan-packages.sh
@@ -10,5 +10,5 @@ bzip2 -5fkv Packages > Packages.bz2
 echo xz compressing
 xz -5fkev Packages > Packages.xz
 
-echo lzma compressing
-xz -5fkev --format=lzma Packages > Packages.lzma
+echo zstd compressing
+zstd -c19 -5fkev Packages > Packages.zst 
